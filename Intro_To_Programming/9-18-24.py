ca7184d3a1604dc569e6784de25522a8b0eb93e7
Ian Holm <ianholm252@gmail.com>
2024-09-18 18:34:45 -0600

    Added english paper and in class file from intro to programming.

---
+++ Intro_To_Programming/9-18-24.py
@@ -0,0 +1,6 @@
+print("Hello world")
+
+f_name = input("What's your first name? ")
+l_name = input("What's your last name? ")
+
+print(f"Hello, {f_name}!")
--- Intro_To_Programming/fav_color.py
+++ Intro_To_Programming/fav_color.py
@@ -1,7 +1,7 @@
 color = input("What's your favorite color? ")
 print(f"{color} is a great choice! My favorite color is green!")
 
-animal = input("What's your favorite mammal? ")
+animal = input("What's your favorite animal? ")
 print(f"{animal} is a great choice! I like bears!!")
 
 food = input("What's your favorite food? ")
