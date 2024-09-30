48c788c8be251e03a50a70dbc8c8ba6370aa4692
Ian Holm <ianholm252@gmail.com>
2024-09-30 11:28:43 -0600

    Updated 9/30/2024 11:30 AM

---
+++ Intro_To_Programming/03_Prepare_Checkpoint.py
@@ -0,0 +1,11 @@
+age = input("How old are you? ")
+print(f"You are {age + 1} years old.")
+
+cartons = input("How many cartons of eggs do you have? ")
+eggs = cartons * 12
+print(f"You have {eggs} eggs.")
+
+people = input("How many people are there? ")
+cookies = input("How many cookies are there? ")
+cookies_per_person = cookies / people
+print(f"There are {cookies_per_person} cookies per person.")
